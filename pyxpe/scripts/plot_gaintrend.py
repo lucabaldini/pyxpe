f51072c1dc8716b66011a6247417da1ece181154
Luca Baldini <luca.baldini@pi.infn.it>
2016-07-05 09:58:57 +0200

    now using 2D hist and projections

--- pyxpe/scripts/plot_gaintrend.py
+++ pyxpe/scripts/plot_gaintrend.py
@@ -11,11 +11,13 @@ G2FWHM = 2.3548200450309493
 tt = ROOT.TChain("tree")
 # SELECT RUNS to be analized
 for runid in [393, 394, 395, 396, 397, 398, 399, 400, 
-              401, 402, 403, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415]:
+              401, 402, 403, 406, 407, 408, 409, 410, 
+              411, 412, 413, 414, 415, 416]:
     tt.Add("/data0/xpe/xpedata/002_%07d/002_%07d_data_TH5.root" %(runid, runid))
 # Note:
 # 1 night gap between run 400 (timeout at 20:11 1/7) and 402 (start 9:37 2/7)
 # 402 ends with timeout, 403 started after ~1 hr -- 403 is very small
+# 1 night gap between run 416 (timeout at 21:15 4/7) and 417 (start 9:13 5/7)
 
 # SELECT OPTIONS
 Label      = 'GPD018_fill1'
@@ -36,6 +38,7 @@ Nbins     = int((maxTime-minTime)/TimeBin)
 print "Eval trend from %f hr to %f hr after start in %d bins" %\
     (minTime, maxTime, Nbins)
 
+chbins   = np.linspace(0,10000,201)
 timeBins = np.linspace(0,maxTime, Nbins+1)
 timeVal  = 0.5*(timeBins[1:] +timeBins[:-1])
 timeErr  = 0.5*(timeBins[1:] -timeBins[:-1])
@@ -46,18 +49,25 @@ gainErr  = []
 resVal   = []
 resErr   = []
 
+
+# project entire tree in a 2D histo
+hAll = ROOT.TH2F("hAll", "hAll", len(chbins)-1, chbins, \
+                 len(timeBins)-1, timeBins)
+thecut =  CUT
+if useSmallSpot:
+    thecut +=  " && " + SmallSpotCut
+print "Projection tree with cut:\n%s" % thecut
+tt.Project("hAll", "%s:fPHeight[0]" % TimeHours, thecut)
 ctmp = ROOT.TCanvas()
 ctmp.Print("tmpAllHisto.ps[");
-htmp = ROOT.TH1F("htmp", "htmp", 200, 0, 10000)
+hAll.Draw("colz")
+ctmp.Print("tmpAllHisto.ps");
+#raw_input("E2go")
+
 g0 = ROOT.TF1("g0", "gaus", 3000, 5000)
 for i in xrange(Nbins):
-    TimeCut = "(%s>=%f && %s <%f)" % \
-              (TimeHours, timeBins[i], TimeHours, timeBins[i+1])
-    thecut =  CUT + "&&"+ TimeCut
-    if useSmallSpot:
-        thecut +=  " && " + SmallSpotCut
-    print i, thecut
-    tt.Project("htmp", "fPHeight[0]", thecut)
+    # get projection in time (y coord in hAll)
+    htmp = hAll.ProjectionX("htmp", i+1,i+1)
     if htmp.GetEntries() >= minEvtInHist:
         htmp.Draw()
         htmp.Fit("g0", "RNQ")
@@ -69,7 +79,8 @@ for i in xrange(Nbins):
         timeXErr.append(timeErr[i])
         gPeak  = g.GetParameter(1)
         gSigma = g.GetParameter(2)
-        print gPeak, gSigma, "Eres=", gSigma/gPeak, "FWHM", 2.355*(gSigma/gPeak)
+        print i, gPeak, gSigma, "Eres=", gSigma/gPeak, \
+            "FWHM", 2.355*(gSigma/gPeak), "\n"
         gainVal.append(gPeak)
         gainErr.append(g.GetParError(1))
         resVal.append(100.*gSigma/gPeak)
@@ -106,13 +117,16 @@ gGain.GetXaxis().SetTimeFormat("#splitline{%d/%m/%y}{%H:%M:%S}");
 gGain.GetXaxis().SetTimeOffset(TimeOffset)
 gGain.GetXaxis().SetLabelOffset(0.03)
 gGain.Draw("ap")
-gGain.Fit("pol1")
+fGain = ROOT.TF1('fGain', 'pol1', 0, 1000)
+fGain.SetParameters(10, 0.002)
+gGain.Fit("fGain")
 ROOT.gPad.Update()
 psGain = gGain.GetListOfFunctions().FindObject("stats");
 psGain.SetX1NDC(0.65);
 psGain.SetY1NDC(0.18);
 psGain.SetX2NDC(0.87);
 psGain.SetY2NDC(0.34);
+ROOT.gPad.Update()
 cTrend.cd(2)
 gRes = ROOT.TGraphErrors(N, timeX, resVal, timeXErr, resErr)
 gRes.SetMarkerStyle(20)
@@ -122,12 +136,13 @@ if useFWHM:
 gRes.GetXaxis().SetTitle("Elapsed Time (hours)")
 gRes.GetYaxis().SetTitle("#Delta E/E (%)")
 gRes.Draw("ap")
-gRes.Fit("pol1")
+fRes = ROOT.TF1('fRes', 'pol1', 0, 1000)
+fRes.SetParameters(0.1, 0.001)
+gRes.Fit("fRes")
 ROOT.gPad.Update()
 psRes = gRes.GetListOfFunctions().FindObject("stats");
 psRes.SetX1NDC(0.65);
 psRes.SetY1NDC(0.18);
 psRes.SetX2NDC(0.87);
 psRes.SetY2NDC(0.34);
-
 ROOT.gPad.Update()
