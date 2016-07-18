7ea1f048844f643990e22c954072f5cf9fc40cc2
Luca Baldini <luca.baldini@pi.infn.it>
2016-07-18 16:22:34 +0200

    Initial import.

---
+++ pyxpe/scripts/create_pixmaps.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# Copyright (C) 2007--2016 the X-ray Polarimetry Explorer (XPE) team.
+#
+# For the license terms see the file LICENSE, distributed along with this
+# software.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+from pyxpe.recon.xpol import xpeXpolMatrix, XPOL_COORDINATE_SYSTEMS
+
+matrix = xpeXpolMatrix()
+for coordinate_system in XPOL_COORDINATE_SYSTEMS:
+    filePath = 'pixmap_%s.dat' % (coordinate_system)
+    matrix.write_pixmap_ascii(filePath, coordinate_system)
+    filePath = 'pixmap_%s.fits' % (coordinate_system)
+    matrix.write_pixmap_fits(filePath, coordinate_system)
