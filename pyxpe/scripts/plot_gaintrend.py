7ef056b40a6e22cfd0c033f3671df96a90989b4c
Luca Baldini <luca.baldini@pi.infn.it>
2016-09-26 12:30:24 +0200

    start trending for GPD19

--- pyxpe/scripts/plot_gaintrend.py
+++ pyxpe/scripts/plot_gaintrend.py
@@ -10,12 +10,14 @@ G2FWHM = 2.3548200450309493
 
 tt = ROOT.TChain("tree") 
 # SELECT RUNS to be analized
+# GPD18
 for runid in [393, 394, 395, 396, 397, 398, 399, 400,
               401, 402, 403, 406, 407, 408, 409, 410, 
               411, 412, 413, 414, 415, 416,
               417, 418, 420, 421, 422, 423, 424, 425, 426, 427, 428,
-              432, 433, 434, 435, 436]:
-    tt.Add("/data1/xpe/xpedata/002_%07d/002_%07d_data_TH5.root" %(runid, runid))
+              432, 433, 434, 435, 436, 437, 438, 439, 440]:
+    pass
+    #tt.Add("/data1/xpe/xpedata/002_%07d/002_%07d_data_TH5.root"%(runid, runid))
 # Note:
 # 1 night gap between run 400 (timeout at 20:11 1/7) and 402 (start 9:37 2/7)
 # 402 ends with timeout, 403 started after ~1 hr -- 403 is very small
@@ -24,24 +26,31 @@ for runid in [393, 394, 395, 396, 397, 398, 399, 400,
 # CHANGE SOURCE POSITION since run 432 (fTimeStamp threshold 1467898100.)
 # 432 ends with timeout,  about 1.5hr gap (16:22 - 17:50) before 434 starts
 
+# SELECT RUNS to be analized
+# GPD19
+for runid in [2724]:
+    tt.Add("/data1/xpe/xpedata/002_%07d/002_%07d_data_TH5.root"%(runid, runid))
+
 # SELECT OPTIONS
 Label      = 'GPD018_fill1'
 TimeBin    = 4. #hours
 nsigfit    = 1.5
-dh         = ROOT.TDatime(2016,6,30,10,56,21) # started on Jun 30 10:56:21 2016
+#dh         = ROOT.TDatime(2016,6,30,10,56,21) # started on Jun 30 10:56:21 2016
+dh         = ROOT.TDatime(2016,9,26,10,59,0) # started on Mon Sep 26 10:59:13 2016
 TimeOffset = dh.Convert()
 #CUT        = '(fTrigWindow > 600*(1 + (fTimeStamp - %f)*0.5e-03/4000.))' % TimeOffset
 CUT = '(1)'
 TimeHours  = "((fTimeStamp-%.1f)/3600.)" % TimeOffset
 minEvtInHist = 500
-useFWHM      = False
-useSmallSpot = True
+useFWHM      = True
+useSmallSpot = False
 SpotCut_tthr = 1467898100
 SpotCut_0    = "((fImpactY[0]-0)**2 + (fImpactX[0]-0.5)**2)<(1.**2)" # max 2.5 to avoid bad chan area
 SpotCut_1    = "((fImpactY[0]+4.)**2 + (fImpactX[0]+4.)**2)<(1.**2)" #
 SmallSpotCut = "((fTimeStamp<=%d)*(%s) + (fTimeStamp>%d)*(%s))" %\
                (SpotCut_tthr, SpotCut_0, SpotCut_tthr, SpotCut_1)
 
+"""
 # time bins logic has to take into account time threshold
 if tt.GetMaximum("fTimeStamp")>=SpotCut_tthr:
     split_time_flag = True
@@ -67,7 +76,12 @@ else:
 
 print SpotCut_tthr, (SpotCut_tthr-TimeOffset)/3600.
 print timeBins
-
+"""
+split_time_flag = False
+minTime    = (tt.GetMinimum("fTimeStamp")-TimeOffset)/3600.
+maxTime    = (tt.GetMaximum("fTimeStamp")-TimeOffset)/3600.
+Nbins      = max(int((maxTime-minTime)/TimeBin), 1)
+timeBins   = np.linspace(0,maxTime, Nbins+1)
 print "Eval trend from %f hr to %f hr after start in %d bins" %\
     (min(timeBins), max(timeBins), len(timeBins))
 
@@ -156,13 +170,13 @@ gGain.GetXaxis().SetLabelOffset(0.03)
 gGain.Draw("ap")
 fGain = ROOT.TF1('fGain', 'pol1', 0, (SpotCut_tthr-TimeOffset))
 fGain.SetParameters(10, 0.002)
-gGain.Fit("fGain", "R")
+#gGain.Fit("fGain", "R")
 ROOT.gPad.Update()
-psGain = gGain.GetListOfFunctions().FindObject("stats");
-psGain.SetX1NDC(0.65);
-psGain.SetY1NDC(0.18);
-psGain.SetX2NDC(0.87);
-psGain.SetY2NDC(0.34);
+#psGain = gGain.GetListOfFunctions().FindObject("stats");
+#psGain.SetX1NDC(0.65);
+#psGain.SetY1NDC(0.18);
+#psGain.SetX2NDC(0.87);
+#psGain.SetY2NDC(0.34);
 ROOT.gPad.Update()
 if split_time_flag:
     time_bound_line = ROOT.TLine()
@@ -186,13 +200,13 @@ gRes.GetYaxis().SetTitle("#Delta E/E (%)")
 gRes.Draw("ap")
 fRes = ROOT.TF1('fRes', 'pol1', 0, (SpotCut_tthr-TimeOffset)/3600.)
 fRes.SetParameters(0.1, 0.001)
-gRes.Fit("fRes", "R")
+#gRes.Fit("fRes", "R")
 ROOT.gPad.Update()
-psRes = gRes.GetListOfFunctions().FindObject("stats");
-psRes.SetX1NDC(0.33);
-psRes.SetY1NDC(0.64);
-psRes.SetX2NDC(0.54);
-psRes.SetY2NDC(0.80);
+#psRes = gRes.GetListOfFunctions().FindObject("stats");
+#psRes.SetX1NDC(0.33);
+#psRes.SetY1NDC(0.64);
+#psRes.SetX2NDC(0.54);
+#psRes.SetY2NDC(0.80);
 cTrend.Update()
 if split_time_flag:
     time_bound_line.DrawLine((SpotCut_tthr-TimeOffset)/3600.,
